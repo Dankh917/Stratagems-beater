b9c415b6d3aefc8175c861916bb51c3889e1883b
dan khilkevich <danchuk213@gmail.com>
2024-03-29 16:18:46 +0300

    figured out a way to get input using C program

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
+#include <Windows.h>
 #include <stdio.h>
 
+void main()
+{
+    // This structure will be used to create the keyboard
+    // input event.
+    INPUT ip;
 
-void main() {
-	
+    // Pause for 5 seconds.
+    Sleep(2000);
 
+    // Set up a generic keyboard event.
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wScan = 0; // hardware scan code for key
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
 
+    // Press the "A" key
+    ip.ki.wVk = 0x41; // virtual-key code for the "a" key
+    ip.ki.dwFlags = 0; // 0 for key press
+    SendInput(1, &ip, sizeof(INPUT));
+
+    // Release the "A" key
+    ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+    SendInput(1, &ip, sizeof(INPUT));
+
+ 
 }
